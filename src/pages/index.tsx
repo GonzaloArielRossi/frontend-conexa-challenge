3fa354725cfb4d37e32551215b5a8f9ebaf9d290
gonzaloarielrossi@gmail.com <gonzaloarielrossi@gmail.com>
2023-10-31 14:55:41 -0300

    Refactor index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,10 @@
-import {
-  Box,
-  Button,
-  Container,
-  SimpleGrid,
-  useColorMode,
-  VStack
-} from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import Head from 'next/head';
 
-import CharacterCards from '@/components/character-cards/CharacterCards';
+import Navbar from '@/components/navbar/Navbar';
+import RickAndMortyEpisodePicker from '@/components/rick-and-morty-episode-picker/RickAndMortyEpisodePicker';
 
 export default function Home() {
-  const { colorMode, toggleColorMode } = useColorMode();
-
   return (
     <>
       <Head>
@@ -21,21 +13,16 @@ export default function Home() {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link href="/favicon.ico" rel="icon" />
       </Head>
-      <Container as={'main'} maxW={'1400px'} mx={'auto'}>
-        <Button onClick={toggleColorMode}>
-          Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-        </Button>
-        <VStack w={'100%'}>
-          <SimpleGrid as={'section'} columns={2} spacing={4} w={'100%'}>
-            <CharacterCards />
-            <CharacterCards />
-          </SimpleGrid>
-          <SimpleGrid as={'section'} columns={3} spacing={10} w={'100%'}>
-            <Box as={'article'}>episodes character 1</Box>
-            <Box as={'article'}>episodes shared</Box>
-            <Box as={'article'}>episodes character 2</Box>
-          </SimpleGrid>
-        </VStack>
+      <Container
+        as={'main'}
+        maxW={'1400px'}
+        mx={'auto'}
+        sx={{
+          scrollbarGutter: 'stable'
+        }}
+      >
+        <Navbar />
+        <RickAndMortyEpisodePicker />
       </Container>
     </>
   );
