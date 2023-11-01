3e1dab2f7249da13c2e6cdb61fd9604642b450f3
gonzaloarielrossi@gmail.com <gonzaloarielrossi@gmail.com>
2023-11-01 17:17:08 -0300

    Modify design

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { Center, Image } from '@chakra-ui/react';
 
 export default function Navbar() {
   return (
-    <Center as={'nav'} pb={16} pt={8}>
+    <Center as={'nav'} py={8}>
       <Image alt={'Rick and Morty logo'} h={'60px'} src={'/images/logo.png'} />
     </Center>
   );
