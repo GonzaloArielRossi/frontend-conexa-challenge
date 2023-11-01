509b757229e12fb0bef67a1c3510f1edad607b29
gonzaloarielrossi@gmail.com <gonzaloarielrossi@gmail.com>
2023-11-01 17:24:55 -0300

    Fix design issue

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { Center, Image } from '@chakra-ui/react';
 
 export default function Navbar() {
   return (
-    <Center as={'nav'} py={8}>
+    <Center as={'nav'} py={6}>
       <Image alt={'Rick and Morty logo'} h={'60px'} src={'/images/logo.png'} />
     </Center>
   );
