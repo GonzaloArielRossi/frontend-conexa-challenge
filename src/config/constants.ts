ae64e3fb9bdec0122d5e85948d7c7e655ffb2485
gonzaloarielrossi@gmail.com <gonzaloarielrossi@gmail.com>
2023-11-01 17:17:16 -0300

    Add character constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,3 +6,11 @@ export const API_ENDPOINTS = {
   LOCATION: 'https://rickandmortyapi.com/api/location/[locationId]',
   LOCATIONS: 'https://rickandmortyapi.com/api/location'
 };
+
+export const CHARACTER_PANELS = ['leftPanel', 'rightPanel'];
+
+export const CHARACTER_STATUSES = {
+  ALIVE: 'Alive',
+  DEAD: 'Dead',
+  UNKNOWN: 'unknown'
+};
