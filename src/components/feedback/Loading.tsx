1e976dfd65062d9a2a86f8210f4ad2edf1228012
gonzaloarielrossi@gmail.com <gonzaloarielrossi@gmail.com>
2023-11-04 15:42:20 -0300

    Change design for loader

--- src/components/feedback/Loading.tsx
+++ src/components/feedback/Loading.tsx
@@ -1,12 +1,15 @@
 import { Center, Spinner } from '@chakra-ui/react';
 
-export default function Loading() {
+type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+
+export default function Loading({ size = 'xl' }: { size?: Size }) {
   return (
     <Center h={'100%'} w={'100%'}>
       <Spinner
         color="brandGreen.500"
         emptyColor="brandCyan.500"
-        size="xl"
+        my={4}
+        size={size}
         speed="0.65s"
         thickness="4px"
       />
