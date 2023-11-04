77a0aa13ac7c1b7705e2cfa6eefd2ba3ade7cfea
gonzaloarielrossi@gmail.com <gonzaloarielrossi@gmail.com>
2023-11-04 15:42:32 -0300

    Add ratings api endpoint

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,6 +2,8 @@ export const API_ENDPOINTS = {
   CHARACTER: 'https://rickandmortyapi.com/api/character/[characterId]',
   CHARACTERS: 'https://rickandmortyapi.com/api/character',
   EPISODE: 'https://rickandmortyapi.com/api/episode/[episodeId]',
+  EPISODE_RATING:
+    'https://api.themoviedb.org/3/tv/60625/season/[seasonId]/episode/[episodeId]',
   EPISODES: 'https://rickandmortyapi.com/api/episode',
   LOCATION: 'https://rickandmortyapi.com/api/location/[locationId]',
   LOCATIONS: 'https://rickandmortyapi.com/api/location'
