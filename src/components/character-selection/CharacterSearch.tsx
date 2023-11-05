d5652c9d29df0b2af6e1c70d6a463f5b7f25da82
gonzaloarielrossi@gmail.com <gonzaloarielrossi@gmail.com>
2023-11-05 14:42:45 -0300

    Make search function de-select characters

--- src/components/character-selection/CharacterSearch.tsx
+++ src/components/character-selection/CharacterSearch.tsx
@@ -1,34 +1,44 @@
-import { Input } from '@chakra-ui/react';
+import { Box, Input } from '@chakra-ui/react';
 import debounce from 'just-debounce-it';
 import { ChangeEvent, useCallback, useState } from 'react';
 
+import { useSelectedCharactersStore } from '@/store/useSelectedCharactersStore';
+
 export default function CharacterSearch({
+  characterPanelId,
   onSearchTermChange
 }: {
   onSearchTermChange: (searchTerm: string) => void;
+  characterPanelId: number;
 }) {
+  const setSelectedCharacters = useSelectedCharactersStore(
+    (state) => state.setSelectedCharacters
+  );
+
   const onSearch = useCallback(
-    debounce((query: string) => onSearchTermChange(query), 300),
+    debounce((query: string) => {
+      setSelectedCharacters(characterPanelId, undefined);
+      onSearchTermChange(query);
+    }, 300),
     []
   );
   const [inputValue, setInputValue] = useState('');
+
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setInputValue(value);
     onSearch(value);
   };
   return (
-    <>
+    <Box h={'40px'} w={{ base: '100%', md: '50%' }}>
       <Input
-        h={'40px'}
         mr={'auto'}
         name="search"
         placeholder="Rick, Morty, Beth..."
         type="search"
         value={inputValue}
-        w={{ base: '100%', md: '50%' }}
         onChange={handleInputChange}
       />
-    </>
+    </Box>
   );
 }
